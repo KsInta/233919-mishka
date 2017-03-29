0eb5056437726bc754af045e34442f880f6c0289
Vitaly Zhitnik <Ks.Inta@mail.ru>
2017-03-29 19:59:21 +0400

    Правки

--- js/catalog.js
+++ js/catalog.js
@@ -7,32 +7,32 @@ var popup_toy_basket = document.querySelector('.form--popup-toy-basket');
 
 var overlay = document.querySelector('.overlay');
 
-order_rabbit.addEventListener("click", function(event) {
+order_rabbit.addEventListener('click', function(event) {
   event.preventDefault();
   popup_rabbit.classList.add('form--popup-display');
   overlay.classList.add('overlay--display');
 });
 
-order_laundry_basket.addEventListener("click", function(event) {
+order_laundry_basket.addEventListener('click', function(event) {
   event.preventDefault();
   popup_laundry_basket.classList.add('form--popup-display');
   overlay.classList.add('overlay--display');
 });
 
-order_toy_basket.addEventListener("click", function(event) {
+order_toy_basket.addEventListener('click', function(event) {
   event.preventDefault();
   popup_toy_basket.classList.add('form--popup-display');
   overlay.classList.add('overlay--display');
 });
 
-overlay.addEventListener("click", function(event) {
+overlay.addEventListener('click', function(event) {
   popup_rabbit.classList.remove('form--popup-display');
   popup_laundry_basket.classList.remove('form--popup-display');
   popup_toy_basket.classList.remove('form--popup-display');
   overlay.classList.remove('overlay--display');
 })
 
-window.addEventListener("keydown", function(event) {
+window.addEventListener('keydown', function(event) {
   if (event.keyCode === 27) {
     popup_rabbit.classList.remove('form--popup-display');
     popup_laundry_basket.classList.remove('form--popup-display');
--- js/product-of-week.js
+++ js/product-of-week.js
@@ -9,12 +9,12 @@ order_product_of_week.addEventListener("click", function(event) {
   overlay.classList.add('overlay--display');
 });
 
-overlay.addEventListener("click", function(event) {
+overlay.addEventListener('click', function(event) {
   popup_product_of_week.classList.remove('form--popup-display');
   overlay.classList.remove('overlay--display');
 })
 
-window.addEventListener("keydown", function(event) {
+window.addEventListener('keydown', function(event) {
   if (event.keyCode === 27) {
     popup_product_of_week.classList.remove('form--popup-display');
     overlay.classList.remove('overlay--display');
